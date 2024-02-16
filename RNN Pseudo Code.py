7c1161590d51af56532e7f32d4b83e864328e89b
BHUKYA VEERENDAR <111682206+BVR143@users.noreply.github.com>
2024-02-16 12:44:20 +0530

    Update RNN Pseudo Code.py

--- RNN Pseudo Code.py
+++ RNN Pseudo Code.py
@@ -1,14 +1,12 @@
 # let us define a RNN class 
 import tensorflow as tf 
 class MyRNNCell(tf.keras.layers.layer):
-  def __init__(self, input_dim,rnn_units):
+  def __init__(self, input_dim,rnn_units, output_dim):
     # Initialize weights and biases for input-to-hidden connection
     # Initialize the Weights and biases for hidden-to-hidden connection
     # Initialize the Weights and Biases for hidden-to-output connection
-    self.W_xh = self.add_weights()
-    self.W_hh = self.add_weights()
-    self.W_hy= self.add_weights()
-
-    
+    self.W_xh = self.add_weights([input_dim, rnn_units])
+    self.W_hh = self.add_weights([rnn_units, rnn_uits])
+    self.W_hy= self.add_weights([rnn_units, output_dim])
 
     
